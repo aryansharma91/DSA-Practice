eed07966bc2dd2e1f066372ef41d4e6358b36160
Aryan Sharma <aryansharma9100ji@gmail.com>
2026-03-30 22:08:40 +0530

    Added Fibonacci function code

---
+++ fibonacci_no_using_function.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+// Function to generate Fibonacci
+void fibonacci(int n) {
+    int t1 = 0, t2 = 1, nextTerm;
+
+    for(int i = 1; i <= n; i++) {
+        cout << t1 << " ";
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+    }
+}
+
+int main() {
+    int n;
+
+    cout << "Enter number of terms: ";
+    cin >> n;
+
+    cout << "Fibonacci Series: ";
+    fibonacci(n);   // function call
+
+    return 0;
+}
