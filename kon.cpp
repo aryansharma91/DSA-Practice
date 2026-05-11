6beaa029620c4c505c95aa21f00f7b92bc5d5226
Aryan Sharma <aryansharma9100ji@gmail.com>
2026-05-11 22:28:05 +0530

    commit dsa

---
+++ kon.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[7]={1,2,3,4,5,6,7};
+    int n = 7;
+    int sum = 0;
+    for(int i =0 ; i< n; i++){
+        sum = sum ^ arr[i];
+
+    }
+    cout<<"The unique number is:" <<sum<<endl;
+    return 0;
+}
